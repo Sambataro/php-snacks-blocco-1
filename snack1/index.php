2622561398e7b4df2c6d54f29eaf5be19882529d
fortunato <sambataro1990@gmail.com>
2021-01-20 15:34:08 +0100

    migliorato con ciclo for snack 1

--- snack1/index.php
+++ snack1/index.php
@@ -13,50 +13,50 @@ Olimpia Milano - Cantù | 55 - 60
 */
 
 $matches = [
-  "match-one" => [
+  [
     "home" => [
-      "Nets",
-      "130",
+      "team" => "Nets",
+      "score" => "130",
     ],
     "guest" => [
-      "Jazz",
+      "team" => "Jazz",
       "95",
     ],
 
   ],
 
-  "match-two" => [
+  [
     "home" => [
-      "Griezzlies",
-      "92",
+      "team" => "Griezzlies",
+      "score" => "92",
     ],
     "guest" => [
-      "Lakers",
-      "95",
+      "team" => "Lakers",
+      "score" => "95",
     ],
 
   ],
 
-  "match-three" => [
+  [
     "home" => [
-      "Raptor",
-      "85",
+      "team" => "Raptor",
+      "score" => "85",
     ],
     "guest" => [
-      "Chicago Bulls",
-      "110",
+      "team" => "Chicago Bulls",
+      "score" => "110",
     ],
 
   ],
 
-  "match-four" => [
+  [
     "home" => [
-      "Celtics",
-      "115",
+      "team" => "Celtics",
+      "score" => "115",
     ],
     "guest" => [
-      "Clippers",
-      "95",
+      "team" => "Clippers",
+      "score" => "95",
     ],
 
   ],
@@ -75,50 +75,16 @@ $matches = [
 
 
      <ul>
-       <li>
-         <?php
-         echo $matches["match-one"]["home"][0] . " - " . $matches["match-one"]["guest"][0]
-          ?>
-          <span>
-           |
-          <?php
-          echo $matches["match-one"]["home"][1] . " - " . $matches["match-one"]["guest"][1]
-           ?>
-          </span>
-       </li>
-       <li>
-         <?php
-         echo $matches["match-two"]["home"][0] . " - " . $matches["match-two"]["guest"][0]
-          ?>
-          <span>
-           |
-          <?php
-          echo $matches["match-two"]["home"][1] . " - " . $matches["match-two"]["guest"][1]
-           ?>
-          </span>
-       </li>
-       <li>
-         <?php
-         echo $matches["match-three"]["home"][0] . " - " . $matches["match-three"]["guest"][0]
-          ?>
-          <span>
-           |
-          <?php
-          echo $matches["match-three"]["home"][1] . " - " . $matches["match-three"]["guest"][1]
-           ?>
-          </span>
-       </li>
-       <li>
-         <?php
-         echo $matches["match-four"]["home"][0] . " - " . $matches["match-four"]["guest"][0]
-          ?>
-          <span>
-           |
-          <?php
-          echo $matches["match-four"]["home"][1] . " - " . $matches["match-four"]["guest"][1]
-           ?>
-          </span>
-       </li>
+       <?php
+        for($i = 0; $i < count($matches); $i++) {
+          $homeSide = $matches[$i]["home"]["team"];
+          $guestSide = $matches[$i]["guest"]["team"];
+          $homeScore = $matches[$i]["home"]["score"];
+          $guestScore = $matches[$i]["guest"]["score"];
+
+          echo "<li>".$homeSide." - ".$guestSide." | ".$homeScore." - ".$guestScore."</li>";
+        }
+      ?>
 
 
      </ul>
